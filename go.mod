46fd265b89a0f3745f33b4ea04b7afa75405a096
Reece Rose <reecerose18@gmail.com>
2021-06-16 22:06:50 -0400

    Added sqlite DB support. Now opening database connection

--- go.mod
+++ go.mod
@@ -1,8 +1,9 @@
-module github.com/reece/rose/go-jwt-auth
+module github.com/reecerose/go-jwt-auth
 
 go 1.16
 
 require (
 	github.com/gofiber/fiber/v2 v2.13.0 // indirect
+	gorm.io/driver/sqlite v1.1.4 // indirect
 	gorm.io/gorm v1.21.11 // indirect
 )
--- main.go
+++ main.go
@@ -2,9 +2,16 @@ package main
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
 )
 
 func main() {
+	_, err := gorm.Open(sqlite.Open("jwt.db"), &gorm.Config{})
+	if err != nil {
+		panic("Could not connect to database")
+	}
+
 	app := fiber.New()
 
 	app.Get("/", func(c *fiber.Ctx) error {
